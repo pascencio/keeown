76d5bfe52cdd18c3f611c9543e97cf97fe3c7843
Patricio Ascencio <patricio.ascencio@karibu.cl>
2020-07-24 05:41:21 -0400

    [FIX]: Start shellscript

--- shell/start.sh
+++ shell/start.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-if [ ! -f ~/.bashrc ];
-then
-    touch ~/.bashrc
-fi
-if [ ! -d .nvm ];
-then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-    source ~/.bashrc
-    nvm install npm
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+. ~/.bashrc
+nvm install node
 cd src/ui
 npm install
 npm run build
