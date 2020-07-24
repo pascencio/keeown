0eeab201295d112e79ec695bf8a0dc61e1fd22de
Patricio Ascencio <patricio.ascencio@karibu.cl>
2020-07-24 05:26:54 -0400

    [FIX]: Reactor

--- shell/start.sh
+++ shell/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+if [ ! -f ~/.bashrc ];
+then
+    touch ~/.bashrc
+fi
+if [ ! -d .nvm ];
+then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    source ~/.bashrc
+    nvm install npm
+fi
 cd src/ui
 npm install
 npm run build
