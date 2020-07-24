5cebc9bd5e91bd4879bba7d7c97041390844e67e
Patricio Ascencio <patricio.ascencio@karibu.cl>
2020-07-24 04:56:10 -0400

    [FIX]: Start script paths

--- shell/start.sh
+++ shell/start.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
-go build -o ./bin/keeown -v ../src/api
-if [ ! -f ~/.bashrc ];
-then
-    touch ~/.bashrc
-fi
+go build -o ~/bin/keeown -v ~/src/api
 if [ ! -d .nvm ];
 then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    source ~/.bashrc
     nvm install npm
 fi
-source ~/.bashrc
 cd src/ui
 npm install
 npm run build
 cd ~
-./bin/keeown
+bin/keeown
