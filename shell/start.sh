0cb5b077a155dad27e1925d81dfeba3d78e283f6
Patricio Ascencio <patricio.ascencio@karibu.cl>
2020-07-24 05:51:00 -0400

    [FIX]: Start keeown before build ui

--- shell/start.sh
+++ shell/start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+bin/keeown &
+mkdir src/ui/dist
+cp index.html src/ui/dist
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 . ~/.bashrc
 nvm install node
 cd src/ui
 npm install
 npm run build
-cd ~
-bin/keeown
